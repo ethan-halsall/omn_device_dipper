48d280cb6dd2e7f08d1923ac020bcf5d4f143c7b
1nfrag <sil3ntcll@gmail.com>
2019-03-08 15:28:12 +0800

    dipper: Set device stock props
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- device-props.mk
+++ device-props.mk
@@ -1,3 +1,11 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.audio.soundfx.type=mi
+
+#Assistant
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true 
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.dist=45 \
@@ -5,16 +13,29 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.sat.fallback.luxindex=405 \
     persist.camera.sat.fallback.lux.d=20
 
-# Display density
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=420
-
 # Display features
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.displayfeature.histogram.enable=true \
     ro.eyecare.brightness.threshold=3 \
-    ro.eyecare.brightness.level=8
+    ro.eyecare.brightness.level=8 \
+    ro.sf.lcd_density=420 \
+    ro.hist.brightness.threshold=5
 
-#Assistant
+# Display postprocessing
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opa.eligible_device=true 	    
+    vendor.display.enable_default_color_mode=36
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fp.fpc=true \
+    ro.hardware.fp.goodix=true
+
+# GNSS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.flp=brcm \
+    ro.hardware.gps=brcm
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.nfc_nci=nqx.default \
+    ro.se.type=HCE,UICC
